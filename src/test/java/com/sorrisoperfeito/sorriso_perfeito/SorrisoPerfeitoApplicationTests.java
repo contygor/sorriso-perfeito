0187bb16552aae0ddada18347ddd1bc46d6f072d
Ygor Araujo <araujo_contato@outlook.com>
2025-05-23 22:35:06 -0300

    Estrutura inicial

---
+++ src/test/java/com/sorrisoperfeito/sorriso_perfeito/SorrisoPerfeitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorrisoperfeito.sorriso_perfeito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorrisoPerfeitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
